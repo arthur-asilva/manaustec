6c889b5a7e17b2e8fc21908d20dc35fdb6734b85
arthur-asilva <dev.arthur.silva@gmail.com>
2022-08-06 16:47:39 -0300

    create profile model

--- core/apps/profiles/models.py
+++ core/apps/profiles/models.py
@@ -1,3 +1,8 @@
+from operator import mod
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    modules = ArrayField(models.CharField(max_length=100))
+    allowed_areas = ArrayField(ArrayField(models.CharField(max_length=100)))
