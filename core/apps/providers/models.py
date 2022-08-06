de6a456802572443aba3ca57de332dd8c9c0bc53
arthur-asilva <dev.arthur.silva@gmail.com>
2022-08-06 17:45:08 -0300

    create provider model

--- core/apps/providers/models.py
+++ core/apps/providers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    name = models.CharField(max_length=254)
+    phone_number = models.CharField(max_length=254)
+    address = models.CharField(max_length=254)
+
+    def __str__(self):
+        return f"{self.name}"
--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.profiles',
     'apps.users',
+    'apps.providers',
 ]
 
 MIDDLEWARE = [
