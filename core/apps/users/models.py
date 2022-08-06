0737e663b7efd4e160dd3dd8c282703fcf01b21c
arthur-asilva <dev.arthur.silva@gmail.com>
2022-08-06 17:36:25 -0300

    create user model

--- core/apps/users/models.py
+++ core/apps/users/models.py
@@ -1,3 +1,17 @@
+import email
 from django.db import models
+from django.forms import PasswordInput
+from apps.profiles.models import Profile
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=254)
+    email = models.CharField(max_length=254)
+    password = models.CharField(max_length=10)
+    profile = models.ForeignKey(Profile, related_name="profile", on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        is_active = 'Ativo'
+        if not self.is_active:
+            is_active = 'Inativo'
+        return f"{self.name}, {self.profile.name} ({is_active})."
